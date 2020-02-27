f2eac137f7fd59a1427a62652046214608c749a3
Florian Guitton <f.guitton@imperial.ac.uk>
2020-02-27 21:44:19 +0000

    Cleaning up code duplication

--- src/index.js
+++ src/index.js
@@ -2,47 +2,36 @@
 
 const characterSet = 'ACDEFGHJKNPQRSTVXYZ2345679';
 
-const randomString = (length) => {
+const getRandomString = (length) => {
     let text = '';
-    for (let i = 0; i < length; i++) {
+    for (let i = 0; i < length; i++)
         text += characterSet.charAt(Math.floor(Math.random() * characterSet.length));
-    }
     return text;
 };
 
-const generateCheckCharacter = (input) => {
-
-    let factor = 2;
+const getRemainder = (input, factor) => {
     let sum = 0;
-    const n = characterSet.length;
-
-    // Starting from the right and working leftwards is easier since 
-    // the initial "factor" will always be "2" 
     for (let i = input.length - 1; i >= 0; i--) {
         const codePoint = characterSet.indexOf(input.charAt(i));
         let addend = factor * codePoint;
-
-        // Alternate the "factor" that each "codePoint" is multiplied by
-        factor = (factor == 2) ? 1 : 2;
-
-        // Sum the digits of the "addend" as expressed in base "n"
-        addend = Math.floor(addend / n) + (addend % n);
+        factor = (factor === 2) ? 1 : 2;
+        addend = Math.floor(addend / characterSet.length) + (addend % characterSet.length);
         sum += addend;
     }
+    return sum % characterSet.length;
+};
 
-    // Calculate the number that must be added to the "sum" 
-    // to make it divisible by "n"
-    const remainder = sum % n;
-    const checkCodePoint = (n - remainder) % n;
-
+const getCheckCharacter = (input) => {
+    const remainder = getRemainder(input, 2);
+    const checkCodePoint = (characterSet.length - remainder) % characterSet.length;
     return characterSet.charAt(checkCodePoint);
 };
 
 const generate = (length) => {
     if (length === undefined || length < 0)
         length = 9;
-    const prefix = randomString(length - 1);
-    const check = generateCheckCharacter(prefix);
+    const prefix = getRandomString(length - 1);
+    const check = getCheckCharacter(prefix);
     const id = `${prefix}${check}`;
     if (id.length > length)
         return generate(length);
@@ -50,30 +39,8 @@ const generate = (length) => {
 };
 
 const validate = (input) => {
-
-    let factor = 1;
-    let sum = 0;
-    let n = characterSet.length;
-
-    // Starting from the right, work leftwards
-    // Now, the initial "factor" will always be "1" 
-    // since the last character is the check character
-    for (let i = input.length - 1; i >= 0; i--) {
-        const codePoint = characterSet.indexOf(input.charAt(i));
-        let addend = factor * codePoint;
-
-
-        // Alternate the "factor" that each "codePoint" is multiplied by
-        factor = (factor == 2) ? 1 : 2;
-
-        // Sum the digits of the "addend" as expressed in base "n"
-        addend = Math.floor(addend / n) + (addend % n);
-        sum += addend;
-    }
-
-    const remainder = sum % n;
-
-    return (remainder == 0);
+    const remainder = getRemainder(input, 1);
+    return (remainder === 0);
 };
 
 const createIDHandler = () => ({
