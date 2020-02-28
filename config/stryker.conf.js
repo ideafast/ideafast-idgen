00c6cc5f552f851d829a417477ef7b4d0bf89deb
Florian Guitton <f.guitton@imperial.ac.uk>
2020-02-28 13:15:18 +0000

    Adding stryker mutation testing

---
+++ config/stryker.conf.js
@@ -0,0 +1,14 @@
+module.exports = function (config) {
+    config.set({
+        mutator: 'javascript',
+        packageManager: 'yarn',
+        reporters: ['html', 'clear-text', 'progress', 'dashboard'],
+        testRunner: 'jest',
+        coverageAnalysis: 'off',
+        maxConcurrentTestRunners: 4,
+        timeoutMS: 30000,
+        jest: {
+            enableFindRelatedTests: false
+        }
+    });
+};
