280118546bd65c35953ff221d3634bf30004e6f9
Florian Guitton <f.guitton@imperial.ac.uk>
2020-02-29 11:22:02 +0000

    Adding unit tests for internal functions

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,4 +1,31 @@
 const IDHandler = require('./index');
+const getRandomStringMock = jest.fn();
+
+getRandomStringMock
+    .mockReturnValueOnce('2A')
+    .mockReturnValueOnce('1337')
+    .mockReturnValue('12345678');
+
+test('Exports the right shape', () => {
+    expect(IDHandler.default).toBeDefined();
+    expect(IDHandler.default.generate).toBeDefined();
+    expect(IDHandler.default.validate).toBeDefined();
+    expect(IDHandler.generate).toBeDefined();
+    expect(IDHandler.validate).toBeDefined();
+});
+
+test('Derives correct remainders', () => {
+    const INT_getRemainder = IDHandler.__get__('getRemainder');
+    const samples = ['HT', '4NT4K', 'QRJA4DRS7', 'FGEET9TY7Z6S5ZG'];
+    expect(samples.map((sample) => INT_getRemainder(sample, 2)).join(',')).toBe('9,14,23,10');
+    expect(samples.map((sample) => INT_getRemainder(sample, 1)).join(',')).toBe('0,0,0,0');
+});
+
+test('Retries on incorrect lengths', () => {
+    IDHandler.__Rewire__('getRandomString', getRandomStringMock);
+    expect(IDHandler.generate()).toHaveLength(9);
+    IDHandler.__ResetDependency__('getRandomString');
+});
 
 test('Generates an ID', () => {
     expect(IDHandler.generate()).toHaveLength(9);
